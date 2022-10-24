ea1caeaf99ac1ad3187161a1653cc40a0bf8ecef
Sergey Pozniakevich <ven07@yandex.ru>
2022-10-24 20:10:12 +0700

    with List comprehension

--- Task_001.py
+++ Task_001.py
@@ -4,12 +4,34 @@
 # - 6782 -> 23
 # - 0,56 -> 11  
 
-n = input("Введите дробное: ")
+# Var my
+# n = input("Введите дробное: ")
  
-suma_n = 0
+# suma_n = 0
  
-for figure in n:
-    if figure.isdigit():
-        suma_n += int(figure)
+# for figure in n:
+#     if figure.isdigit():  # insdigit()  -  проверка позиции на цифру
+#         suma_n += int(figure)
  
-print("Сумма цифр равна:", suma_n)
+# print("Сумма цифр равна:", suma_n)
+
+
+# Var 2
+
+# n = list(input("Введите дробное: ")) #
+# sum_dig = 0
+
+# for i in range(len(n)):    # пробегаем по ряду длиной  n
+#     if n[i] == '.':        # если позиция равна "." ->
+#         continue           # -> переходим в начало цикла
+#     sum_dig += int(n[i])
+# print(sum_dig)
+
+# Var with List comprehension
+
+from unicodedata import digit
+
+
+n = list(input("Введите дробное: "))
+sum_1 = sum([int(digit) for digit in n if digit.isdigit()])
+print(sum_1)
